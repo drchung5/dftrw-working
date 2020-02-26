531fb387e0971a1a896ed5f80bf0e586fea9df59
David Chung <dchung@dchung.com>
2020-02-25 22:39:08 -0500

    Removed Closure implementation

--- functional-programming/closures/src/main/java/closures/Closure.java
+++ functional-programming/closures/src/main/java/closures/Closure.java
@@ -5,13 +5,6 @@ import java.util.function.Predicate;
 public class Closure {
 
   public static void main(String[] argv) {
-    Predicate<Integer> lambda = getLambda();
-    System.out.println(lambda.test(3));
-  }
-
-  public static Predicate<Integer> getLambda() {
-    int a = 7; // this variable is 'effectively final'
-    return x -> x == a;
   }
 
 }
